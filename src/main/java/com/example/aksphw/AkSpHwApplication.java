9de04ec57731907a7e87eab1068b713d25a68541
andrzej.rzepecki <arzepecki@bluemercury.com>
2024-10-31 11:05:55 +0100

    init

---
+++ src/main/java/com/example/aksphw/AkSpHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.aksphw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkSpHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkSpHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aksphw/AkSpHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aksphw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkSpHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
